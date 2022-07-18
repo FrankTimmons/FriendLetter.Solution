395498e150d2630f7f1e59544d43a32f8a23f289
FrankTimmons <franktimmonspdx@gmail.com>
2022-07-18 10:57:12 -0700

    Add bonus fields to friendletter
    Co-authored-by: Maxwell Alvord <maxwellalvord@gmail.com>

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -12,21 +12,30 @@ namespace FriendLetter.Controllers
     public string Goodbye() { return "Goodbye friend."; }
 
     [Route("/")]
-    public ActionResult Letter() 
+    public ActionResult Letter()
     {
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
       myLetterVariable.Sender = "Jasmine";
-      return View(myLetterVariable); 
+      myLetterVariable.Location = "Portland";
+      myLetterVariable.IdealLocation = "Mexico";
+      myLetterVariable.Souvenir = "Taco";
+      return View(myLetterVariable);
     }
 
     [Route("/postcard")]
-    public ActionResult Postcard(string recipient, string sender)
+    public ActionResult Postcard(string recipient, string sender, string location, string idealLocation, string souvenir)
     {
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = recipient;
       myLetterVariable.Sender = sender;
+      myLetterVariable.Location = location;
+      myLetterVariable.IdealLocation = idealLocation;
+      myLetterVariable.Souvenir = souvenir;
       return View(myLetterVariable);
     }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
   }
 }
